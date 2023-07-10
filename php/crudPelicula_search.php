7a84a09b73caff2fd403593a9ab50da77a2081cc
Leonardo Berni <leonardoberni0@gmail.com>
2023-07-10 14:21:03 -0300

    Leo añadio todo crudPelicula

---
+++ php/crudPelicula_search.php
@@ -0,0 +1,40 @@
+<?php
+    include('conexion.php');
+    $searchPelicula = trim($_GET['searchPelicula']);
+
+    $consulta = "SELECT * FROM pelicula WHERE nombre LIKE '%$searchPelicula%'";
+    $resultado = mysqli_query($conex,$consulta);
+    
+    if($resultado){
+        foreach($resultado as $fila){
+
+            $id_pelicula = $fila['id_pelicula']; 
+            $nombre = $fila['nombre']; 
+            $sinopsis = $fila['sinopsis']; 
+            $fecha = $fila['fecha']; 
+            $img = $fila['img']; 
+            $generos = $fila['generos']; 
+
+
+            if($img != null){
+                $img = 'SI';
+            }else{
+                $img = 'NO';
+            }
+
+            echo"
+                <tr>
+                    <td>$id_pelicula</td>
+                    <td>$nombre</td>
+                    <td>$sinopsis</td>
+                    <td>$generos</td>
+                    <td>$fecha</td>
+                    <td>$img</td>
+                </tr>
+            ";
+
+        }
+
+    }
+
+?>
